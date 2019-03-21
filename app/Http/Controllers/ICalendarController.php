caa39e7f463d05e412e0642c30ec29096228050d
motoy3d <motoy3d@gmail.com>
2019-03-21 19:08:26 +0900

    iCalで時間が反映されるよう修正。 #24

--- app/Http/Controllers/ICalendarController.php
+++ app/Http/Controllers/ICalendarController.php
@@ -55,7 +55,7 @@ class ICalendarController extends Controller
       if ($schedule->time_from) {
         $start = new Carbon($schedule->schedule_date . ' ' . $schedule->time_from);
       }
-      Log::info('----> start ' . $start . ' ' . $start->getTimezone()->getName());
+//      Log::info('----> start ' . $start . ' ' . $start->getTimezone()->getName());
 
       $end = new Carbon($schedule->schedule_date);
       if ($schedule->time_to) {
