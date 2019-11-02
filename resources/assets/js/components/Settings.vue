208eb8c8ff1fa4b8af7c122892436edf85e0ce4d
motoy3d <motoy3d@gmail.com>
2019-11-02 15:21:48 +0900

    PWA on iOSの場合にLINE連携ができない旨のメッセージを表示 #64

--- resources/assets/js/components/Settings.vue
+++ resources/assets/js/components/Settings.vue
@@ -43,6 +43,9 @@
           </v-ons-list-item>
           <v-ons-list-item >
             <img src="/img/LINE_APP.png" style="width:22px" class="mr-5"> LINEで通知を受け取る
+            <div v-if="isIOSAndPWA">
+              ホーム画面のアイコンから起動している場合は設定できません。Safariから開いて設定してください。
+            </div>
             <div class="right">
               <v-ons-switch v-model="lineNotificationFlg"
                             @click="updateLINENotificationFlg()"></v-ons-switch>
@@ -118,6 +121,7 @@
         loading: false,
         errored: false,
         posting: false,
+        isIOSAndPWA: this.$ons.platform.isIOS() && location.href.indexOf('launcher=true') != -1
       }
     },
     computed: {
