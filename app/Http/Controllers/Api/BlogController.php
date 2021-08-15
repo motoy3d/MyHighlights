eafb3dd8665b343f6b5cf74cb6c27be2b7f0169e
motoy3d <motoy3d@gmail.com>
2021-08-16 00:31:12 +0900

    Update composer libs, change SES auth to MAIL_DRIVER=ses

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Team;
-use Awjudd\FeedReader\Facades\FeedReader;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Response;
 
